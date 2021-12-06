0485b2140900a954ab18e7e3072de232c1c5dde8
LuisGomez89 <luis.gh8907@gmail.com>
2021-12-06 10:59:06 -0600

    added new files and folders

---
+++ src/components/Recent/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { RecentContainer } from './styled'
+
+export const Recent = () => {
+    return (
+        <RecentContainer>
+            <Text>recent</Text>
+        </RecentContainer>
+    )
+}
---
+++ src/screens/Details.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const Details = () => {
+    return (
+        <View>
+            <Text>DETAILS</Text>
+        </View>
+    )
+}
+
