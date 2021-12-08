b065502ebc93dce220e22265ec522cafded67b8a
Demis Alberto Rincón Martínez <darmfma@gmail.com>
2021-12-07 18:02:10 -0600

    fixes

--- src/library/images/index.js
+++ src/library/images/index.js
@@ -1,8 +1,8 @@
-import Burger from "./burger.png"
-import Ramen from "./ramen.png"
-import Huevo from "./huevo.png"
+import Burger from "./burguer.png"
+import Huevo  from "./huevo.png"
 import Micro from "./micro.png"
 import Pizza from "./pizza.png"
+import Ramen  from "./ramen.png"
 import Sandwich from "./sandwich.png"
 import Search from "./search.png"
 
